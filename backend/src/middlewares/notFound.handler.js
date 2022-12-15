b439ef62fd29113737809dd2920024ff475e3706
Mario2701 <77269474@continental.edu.pe>
2022-12-14 20:15:24 -0500

    fix(backend): Update notFoundMiddelware

--- backend/src/middlewares/notFound.handler.js
+++ backend/src/middlewares/notFound.handler.js
@@ -2,7 +2,6 @@ import { notFound } from "@hapi/boom";
 
 export default function notFoundHandler(req,res,next) { 
     const notFoundBoom = notFound("Ruta no encontrada")
-    console.log("probando")
     res.status(notFoundBoom.output.statusCode).json(
         notFoundBoom.output.payload
     )
