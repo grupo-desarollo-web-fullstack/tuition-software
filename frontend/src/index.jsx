8a946d5cdee80ac717029342d90099c162eea0fb
alejooroncoy <adoa2705@gmail.com>
2022-12-16 07:00:17 -0500

    feat(frontend): Inject styles in index.jsx 🦄👨‍💻✨

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./router/App";
+import "./styles/index.scss";
 
 const rootRef = document.querySelector("#root");
 
