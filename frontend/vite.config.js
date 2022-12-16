d913b32d78c91d8ace44ba03a73462aa37258564
alejooroncoy <adoa2705@gmail.com>
2022-12-16 06:49:47 -0500

    feat(frontend): Update vite.config.js 👨‍💻✨

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  envPrefix: "TS",
   server: {
     watch: {
       usePolling: true,
-    }
+    },
   },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
