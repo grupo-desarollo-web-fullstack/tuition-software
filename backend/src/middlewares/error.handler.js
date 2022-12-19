ccedb7d5d03b88808a29438f076e8a5d44739bb6
alejooroncoy <adoa2705@gmail.com>
2022-12-18 19:38:04 -0500

    fix(backend): Add handle error authenticate 👨‍💻✨

--- backend/src/middlewares/error.handler.js
+++ backend/src/middlewares/error.handler.js
@@ -1,10 +1,16 @@
-import { badImplementation } from "@hapi/boom";
+import { badImplementation, unauthorized } from "@hapi/boom";
 
 function handlerError(err, req, res, next) {
   let error = err;
 
   if (!error.isBoom) {
-    error = badImplementation(error.message);
+    if (error.name === "AuthenticationError") {
+      error = unauthorized(error.message);
+    } else {
+      error = badImplementation(error.message, {
+        stack: error.stack,
+      });
+    }
   }
   const { output } = error;
   res.status(output.statusCode).json({ ...output.payload });
