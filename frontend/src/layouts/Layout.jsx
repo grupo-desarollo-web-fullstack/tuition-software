8449c8b18b842f89bfe2b60a248f2d0460210148
alejooroncoy <adoa2705@gmail.com>
2022-12-21 11:53:12 -0500

    feat(frontend): Add layouts 🌌👨‍💻✨

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,11 +1,34 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import { Outlet, ScrollRestoration } from "react-router-dom";
+import Footer from "@components/sections/Footer";
+import Header from "@components/sections/Header";
+import config from "@config/index";
+import stateUser from "@libs/states/user";
+import getUser from "@libs/auth/getUser";
+import "@styles/modules/layout.scss";
+import "@styles/modules/menu.scss";
+
+export const loaderLayout = async () => {
+  const tokenTuitionSoftware = localStorage.getItem(
+    config.tokenTuitionSoftware
+  );
+  if (tokenTuitionSoftware) {
+    const user = await getUser(tokenTuitionSoftware);
+    if (!user) {
+      localStorage.removeItem(config.tokenTuitionSoftware);
+    } else {
+      stateUser.user = user;
+    }
+  }
+  return null;
+};
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
+      <ScrollRestoration />
     </>
   );
 };
