904fed7fdd05eebbd4a0bcef9928afd700a5bf51
Mario2701 <77269474@continental.edu.pe>
2022-12-21 11:08:50 -0500

    fix(backend):Update estudiante and merge.

--- backend/src/routes/estudiante.routes.js
+++ backend/src/routes/estudiante.routes.js
@@ -5,7 +5,6 @@ import schemaEstudiante from "../schemas/estudiante.schema.js";
 import validatorHandler from "../middlewares/validator.handler.js";
 import estudianteServices from "../services/estudiante.services.js";
 import config from "../config/index.js";
-import { notFound } from "@hapi/boom";
 
 const estudiante = express.Router();
 const options = {
