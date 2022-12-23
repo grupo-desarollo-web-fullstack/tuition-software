62d7c98fc132df26ae1be3bde6ef2a3ad1a8990d
alejooroncoy <adoa2705@gmail.com>
2022-12-23 06:54:14 -0500

    docs(frontend): Add type User 👨‍💻✨

--- frontend/src/hooks/auth/useUser.js
+++ frontend/src/hooks/auth/useUser.js
@@ -4,6 +4,20 @@ import getUser from "@libs/auth/getUser";
 import stateUser from "@libs/states/user";
 import { useSyncExternalStore } from "react";
 
+/**
+ * @typedef User
+ * @property {number} id
+ * @property {string} nombre
+ * @property {string} email
+ * @property {string} carrera
+ * @property {string} foto
+ * @property {number} ciclo
+ */
+
+/**
+ *
+ * @returns {[User, (user: User?) => void]}
+ */
 export default function useUser() {
   const user = useSyncExternalStore(
     stateUser.suscribe.bind(stateUser),
