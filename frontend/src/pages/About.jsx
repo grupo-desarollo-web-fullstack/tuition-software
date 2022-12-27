7a24d9ad01b4a6373eddbbc3267cd510b86913b4
york30 <york.antayhua@gmail.com>
2022-12-26 21:04:44 -0500

    feat(frontend):Updating about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,16 @@
+import "@styles/modules/members.scss"
 const About = () => {
-  return <main className="main">About</main>;
+  return <main className="main main--about">
+    <section className="members">
+      <div className="members-container">
+        <h2 className="members__title">Miembros del Staff</h2>
+      </div>
+    </section>
+
+  </main>;
 };
 
 export default About;
+
+
+// # I will work from this point
